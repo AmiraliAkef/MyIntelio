412c1c15b0232b590729f1b43d6ed12d58b2667a
Amirali Akef <amirali.akef@yahoo.com>
2026-01-06 05:22:36 -0500

    update

--- app/main.py
+++ app/main.py
@@ -5,12 +5,11 @@ from app.controllers.chat import chat_with_amirali
 
 app = FastAPI()
 
-# THE ENDPOINT: This is the bridge between Front and Back
+
 @app.post("/api/chat", response_model=ChatResponse)
 async def chat_endpoint(request: ChatRequest):
     answer =  await  chat_with_amirali(request.question)
     return answer
 
-# THE VIEW: This serves your HTML/CSS/JS files
-# 'html=True' means it will automatically show index.html at http://localhost:8000
+
 app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
