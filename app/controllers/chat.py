8d4d6b2cfc37555177d80e71cdccad65fdc8de67
Amirali Akef <amirali.akef@yahoo.com>
2026-01-06 04:15:57 -0500

    Fix nested venv repo and finalize MVC structure

---
+++ app/controllers/chat.py
@@ -0,0 +1,31 @@
+import ollama
+import os
+from langchain_community.document_loaders import PyPDFLoader
+
+
+async def chat_with_amirali(theQuestoin: str):
+    filePath = "data/Amirali_Resume.pdf"
+    global context
+    if os.path.exists(filePath):
+        loader = PyPDFLoader(filePath)
+        pages = loader.load()
+        # combining pages
+        context = "\n".join([p.page_content for p in pages])
+
+    #  AIs personality
+    system_instructions = f"""
+    You are an AI assistant representing Amirali. 
+    Use the following resume context to answer questions about him. 
+    If the answer isn't in the context, say 'I'm not sure about that, but you can contact Amirali directly.'
+
+    Context:
+    {context}
+    """
+
+    # calling local ai
+    response = ollama.chat(model='llama3.2', messages=[
+        {'role': 'system', 'content': system_instructions},
+        {'role': 'user', 'content': theQuestoin},
+    ])
+
+    return response['message']['content']
---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from app.models.schemas import ChatRequest, ChatResponse
+from app.controllers.chat import chat_with_amirali
+
+app = FastAPI()
+
+# THE ENDPOINT: This is the bridge between Front and Back
+@app.post("/api/chat", response_model=ChatResponse)
+async def chat_endpoint(request: ChatRequest):
+    answer =  await  chat_with_amirali(request.question)
+    return {"answer": answer}
+
+# THE VIEW: This serves your HTML/CSS/JS files
+# 'html=True' means it will automatically show index.html at http://localhost:8000
+app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
